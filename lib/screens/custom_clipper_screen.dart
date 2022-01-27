1f623429579ea5b88c072f0c56b78fe3a354c2af
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-01-27 15:53:53 +0600

    s

--- lib/screens/custom_clipper_screen.dart
+++ lib/screens/custom_clipper_screen.dart
@@ -1,15 +1,37 @@
 import 'package:flutter/material.dart';
 
-class CustomClipperScreen extends StatelessWidget {
+class CustomClipperScreen extends StatefulWidget {
   const CustomClipperScreen({Key? key}) : super(key: key);
+  @override
+  State<CustomClipperScreen> createState() => _CustomClipperScreenState();
+}
+
+class _CustomClipperScreenState extends State<CustomClipperScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      value: 0.0,
+      upperBound: 1.0,
+      lowerBound: -1.0,
+      duration: Duration(seconds: 5),
+    )..repeat();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ClipPath(
-          clipper: MyClipper(),
-          child: Image.asset('assets/brush.jpg', fit: BoxFit.cover),
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, widget) => ClipPath(
+            clipper: MyClipper(value: _controller.value),
+            child: Image.asset('assets/brush.jpg', fit: BoxFit.cover),
+          ),
         ),
       ),
     );
@@ -17,18 +39,22 @@ class CustomClipperScreen extends StatelessWidget {
 }
 
 class MyClipper extends CustomClipper<Path> {
+  final value;
+  MyClipper({required this.value});
+
   @override
   getClip(Size size) {
     final path = Path();
 
-    // // WAVE demo
-    // path.lineTo(size.width, 0);
-    // path.lineTo(size.width, size.height);
-    // final start = Offset(size.width / 2.5, size.height / 2);
-    // final end = Offset(size.width / 6, size.height);
-    // path.quadraticBezierTo(start.dx, start.dy, end.dx, end.dy);
-    // path.lineTo(0, size.height);
-    // path.close();
+    // WAVE demo
+    path.lineTo(size.width, 0);
+    path.lineTo(size.width, size.height);
+    final start =
+        Offset(size.width / 2.5 * value * 20, size.height / 2 * value * 20);
+    final end = Offset(size.width / 6 * value * 10, size.height * value * 10);
+    path.quadraticBezierTo(start.dx, start.dy, end.dx, end.dy);
+    path.lineTo(0, size.height);
+    path.close();
 
     // //poligon with 4 points
     // final points = [
@@ -38,19 +64,19 @@ class MyClipper extends CustomClipper<Path> {
     //   Offset(0, size.height),
     // ];
     // path.addPolygon(points, true);
-    
-    // Poligon with 8 points
-    final points = [
-      Offset(size.width * .30, 0),
-      Offset(size.width*.70, 0),
-      Offset(size.width ,size.height* .30),
-      Offset(size.width, size.height*.70),
-      Offset(size.width*.70, size.height),
-      Offset(size.width*.30, size.height),
-      Offset(0, size.height*.70),
-      Offset(0, size.height*.30),
-    ];
-    path.addPolygon(points, true);
+
+    // // Poligon with 8 points
+    // final points = [
+    //   Offset(size.width * .30, 0),
+    //   Offset(size.width * .70, 0),
+    //   Offset(size.width, size.height * .30),
+    //   Offset(size.width, size.height * .70),
+    //   Offset(size.width * .70, size.height),
+    //   Offset(size.width * .30, size.height),
+    //   Offset(0, size.height * .70),
+    //   Offset(0, size.height * .30),
+    // ];
+    // path.addPolygon(points, true);
     return path;
   }
 
