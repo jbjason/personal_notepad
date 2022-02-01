6bfdf37f638db5b826aa8e0060d6cac53d6031d9
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-02-02 02:34:52 +0600

    Update my_note.dart

--- lib/models/my_note.dart
+++ lib/models/my_note.dart
@@ -2,7 +2,7 @@ import 'package:personal_notepad/models/drawing_area.dart';
 
 class MyNote {
   final String title, description;
-  final DrawingArea points;
+  final List<DrawingArea> points;
   const MyNote({
     required this.title,
     required this.description,
