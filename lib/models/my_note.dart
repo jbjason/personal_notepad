b4104ed028b864c555c0812e25fced5073adaccd
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-02-03 22:34:03 +0600

    Update my_note.dart

--- lib/models/my_note.dart
+++ lib/models/my_note.dart
@@ -6,10 +6,12 @@ class MyNote {
   final String title, description;
   final File? imageDir;
   final List<DrawingArea> points;
+  final DateTime dateTime;
   const MyNote({
     required this.title,
     required this.description,
     required this.points,
     required this.imageDir,
+    required this.dateTime,
   });
 }
