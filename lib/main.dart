0067ef1a57195d4e27052e90c5dfd3d3ef7b64d5
Jubayer <62088928+jbjason@users.noreply.github.com>
2022-02-12 21:41:40 +0600

    s

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.purple,
+          scaffoldBackgroundColor: Colors.grey[850]
         ),
         //home: const TestingScreen(),
         home: const HomeScreen(),
